231563bda2bf5e21a0031fe082aa811686470603
William Dann <williamdann@hotmail.com>
2011-12-01 21:41:03 +0000

    * Config file class done

---
+++ SeasonalGlobe/AppConfig.cpp
@@ -0,0 +1,167 @@
+#include "AppConfig.h"
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include "strutils.h"
+#include "float2.h"
+#include "float3.h"
+#include "float4.h"
+using namespace std;
+
+AppConfig::AppConfig(void)
+{
+}
+
+AppConfig::~AppConfig(void)
+{
+}
+
+bool AppConfig::ParseConfigFile(const std::string &filename)
+{
+	// Read each line and try to parse each into the form "ParameterName = Value"
+	ifstream out(filename);
+	if(!out.good()) { return false; }
+
+	configMap.clear();
+
+	while(!out.eof())
+	{
+		string inp;
+		getline(out, inp);
+
+		if(!inp.size()) continue;
+		if(inp.find('=') == string::npos) continue;
+
+		TrimLeadingWhitespace(inp);
+
+		std::vector<string> res;
+		split(inp, '=', res);
+
+		if(!res.size()) continue;
+		if(!res.front().size() || !res.back().size()) continue;
+		if(res.front()[0] == '#') continue;
+		
+		string &paramName = res.front();
+		string &value = res.back();
+
+		TrimLeadingWhitespace(paramName);
+		TrimLeadingWhitespace(value);
+		// dont trim trailing whitespace from value, as it may be a string that needs it
+		TrimTrailingWhitespace(paramName);
+
+		// If we get here there is a valid parameter name and value with an equals symbol seperating them
+		configMap[paramName] = value;
+	}
+
+	out.close();
+
+	return true;
+};
+
+bool AppConfig::ConfigParameterExists(const std::string &param)
+{
+	return configMap.count(param) > 0;
+};
+
+bool AppConfig::GetString(const std::string &param, std::string &out)
+{
+	if(ConfigParameterExists(param))
+	{
+		out = configMap[param];
+		return true;
+	}
+	return false;
+};
+
+bool AppConfig::GetInt(const std::string &param, i32 &out)
+{
+	string str;
+	if(GetString(param, str))
+	{
+		TrimTrailingWhitespace(str);
+		i32 ret = atoi(str.c_str());
+		if(ret)
+		{
+			out = ret;
+			return true;
+		}
+	}
+	return false;
+};
+
+bool AppConfig::GetFloat(const std::string &param, f32 &out)
+{
+	string str;
+	if(GetString(param, str))
+	{
+		TrimTrailingWhitespace(str);
+
+		f32 ret=0;
+		stringstream buff(str);
+		buff >> ret;
+		if(!buff.fail())
+		{
+			out = ret;
+			return true;
+		}
+	}
+	return false;
+};
+
+bool AppConfig::GetVec2(const std::string &param, float2 &out)
+{
+	string str;
+	if(GetString(param, str))
+	{
+		TrimTrailingWhitespace(str);
+
+		float2 ret;
+		stringstream buff(str);
+		char open_bracket, comma, close_bracket;
+		buff >> open_bracket >> ret.GetVec()[0] >> comma >> ret.GetVec()[1] >> close_bracket;
+		if(open_bracket != '(' || close_bracket != ')' || comma != ','
+			|| buff.fail()) { return false; }
+		out = ret;
+		return true;
+	}
+	return false;
+};
+
+bool AppConfig::GetVec3(const std::string &param, float3 &out)
+{
+	string str;
+	if(GetString(param, str))
+	{
+		TrimTrailingWhitespace(str);
+
+		float3 ret;
+		stringstream buff(str);
+		char open_bracket, comma, comma2, close_bracket;
+		buff >> open_bracket >> ret.GetVec()[0] >> comma >> ret.GetVec()[1] >> comma2 >> ret.GetVec()[2] >> close_bracket;
+		if(open_bracket != '(' || close_bracket != ')' || comma != ','
+			|| comma2 != ',' || buff.fail()) { return false; }
+		out = ret;
+		return true;
+	}
+	return false;
+};
+
+bool AppConfig::GetVec4(const std::string &param, float4 &out)
+{
+	string str;
+	if(GetString(param, str))
+	{
+		TrimTrailingWhitespace(str);
+
+		float4 ret;
+		stringstream buff(str);
+		char open_bracket, comma, comma2, comma3, close_bracket;
+		buff >> open_bracket >> ret.GetVec()[0] >> comma >> ret.GetVec()[1] >> comma2 >> ret.GetVec()[2] >> 
+			comma3 >> ret.GetVec()[3] >> close_bracket;
+		if(open_bracket != '(' || close_bracket != ')' || comma != ','
+			|| comma2 != ',' || comma3 != ',' || buff.fail()) { return false; }
+		out = ret;
+		return true;
+	}
+	return false;
+};
---
+++ SeasonalGlobe/AppConfig.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include "ctypes.h"
+
+class float2;
+class float3;
+class float4;
+
+// Config file in format:
+// ParameterName =  Value
+// Example: Velocity = (10,20,30)
+//          LSystemRule = (P, F+F[X])
+//          K = 3
+// The AppConfig class is generic and will read the entire config file, with Parameter Names and Values, both
+// as strings. At read time, the AppConfig class will NOT parse the data fully (i.e. it will not create
+// a vector from the Velocity example above). The class will overwrite rather than storing multiple copies of data
+// E.g. if "speed" appears twice, the final value in the map will be the second one
+// Functions are provided to check whether a parameter exists, and to get a parameter as specific type.
+// For example, GetInt() or GetVec4(). See the specific functions for parsing requirements and results
+// Blank space in lines will be safely ignored. Lines starting with "#" will also be ignored as comments
+class AppConfig
+{
+private:
+	std::map<std::string, std::string> configMap; // holds strings for the keys (config parameter names) and results
+
+public:
+	AppConfig(void);
+	~AppConfig(void);
+
+	// Clears configMap then parses filename into configMap (parameterName, value)
+	// Format: ParameterName = Value
+	bool ParseConfigFile(const std::string &filename);
+
+	// returns true if configMap contains the key param
+	bool ConfigParameterExists(const std::string &param);
+
+	// Returns false if key param does not exist, otherwise returns the value in map associated with param
+	bool GetString(const std::string &param, std::string &out);
+
+	// Parses the value of key param as a signed integer
+	// If key param does not exist, it will return false and out  will remain unchanged
+	// If the parsing to an integer fails for whatever reason, it will return false and out will remain unchanged
+	// On success, true will be returned and out will be set to the parsed integer
+	bool GetInt(const std::string &param, i32 &out);
+
+	// Parses the value of key param as a float
+	// If key param does not exist, it will return false and out  will remain unchanged
+	// If the parsing to a float fails for whatever reason, it will return false and out will remain unchanged
+	// On success, true will be returned and out will be set to the parsed float
+	bool GetFloat(const std::string &param, f32 &out);
+
+	// Parses the value of key param as a 2 element vector
+	// If key param does not exist, it will return false and out  will remain unchanged
+	// If the parsing to a vector fails for whatever reason, it will return false and out will remain unchanged
+	// On success, true will be returned and out will be set to the parsed vector
+	// Vector should be in form (x,y). E.g. Velocity = (10,20)
+	// This function could be used for any 2 element value
+	bool GetVec2(const std::string &param, float2 &out);
+
+	// Parses the value of key param as a 3 element vector
+	// If key param does not exist, it will return false and out  will remain unchanged
+	// If the parsing to a vector fails for whatever reason, it will return false and out will remain unchanged
+	// On success, true will be returned and out will be set to the parsed vector
+	// Vector should be in form (x,y,z). E.g. Velocity = (10,20,30)
+	// This function could be used for any 3 element value (including RGB colours, GetVec4 for RGBA)
+	bool GetVec3(const std::string &param, float3 &out);
+	
+	// Parses the value of key param as a 4 element vector
+	// If key param does not exist, it will return false and out  will remain unchanged
+	// If the parsing to a vector fails for whatever reason, it will return false and out will remain unchanged
+	// On success, true will be returned and out will be set to the parsed vector
+	// Vector should be in form (x,y,z,w). E.g. Velocity = (10,20,30,1)
+	// This function could be used for any 4 element value (including RGBA colours)
+	bool GetVec4(const std::string &param, float4 &out);
+};
